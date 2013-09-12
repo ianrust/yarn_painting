734936fa486e297d0e31a26dce4ea3a672ac6b82
Ian Rust <ianrust@google.com>
2013-09-11 19:11:14 -0700

    sped up, need to work on optimization routine

--- ypgen.py
+++ ypgen.py
@@ -1,5 +1,6 @@
-import itertools,sys,argparse,math,Image,time
+import itertools,sys,argparse,math,Image,time,random
 import numpy as np
+from scipy.optimize import minimize
 
 def parseArgs():
 	parser=argparse.ArgumentParser(prog=sys.argv[0])
@@ -15,10 +16,10 @@ def parseArgs():
 def generateDecisionYarns(args,image_grid,image_matrix):
 	peg_grid=generateGrid(args.pegs[0],args.pegs[1],xscale=args.size[1]/args.pegs[1],yscale=args.size[0]/args.pegs[0],edges=True)
 	peg_combos=itertools.combinations(peg_grid,2)
-	yarns = [yarn(combo[0],combo[1],1) for combo in peg_combos]
+	yarns = [yarn(combo[0],combo[1]) for combo in peg_combos if not (combo[0].x==combo[1].x or combo[0].y==combo[1].y)]
 	i=0
 	for yarner in yarns:
-		image_grid=yarner.setContributingPoints(dist=image_matrix.shape[0]/args.image_size,grid=image_grid)
+		image_grid=yarner.setContributingPoints(dist=2*image_matrix.shape[0]/args.image_size,grid=image_grid)
 		i=i+1
 		print str(int(i/float(len(yarns))*100))+"%"
 		# print str(yarner.start)+','+str(yarner.finish)
@@ -44,28 +45,28 @@ def getPerpDistance(point,start,end):
 	distance = math.fabs(Dq*point.x-Dp*point.y+start.x*end.y-start.y*end.x)/math.sqrt(Dp**2+Dq**2)
 	return distance
 
-def getObjectiveFunction(yarns,image_act):
+def getObjectiveFunction(yarn_on_off):
+	global yarns_opt
 	score=0.0
-	for yarn in yarns:
+	for yi,yarn in enumerate(yarns_opt):
 		for point in yarn.cont_points:
-			score=score+math.exp(-(point[1])**2)/(point[0].r+1)# also multiply by difference between actual RGB value and non in exponent
-	return score/(len(yarns)*len(image_act))
+			score=score+round(yarn_on_off[yi])*math.exp(-(point[1])**2)# also multiply by difference between actual RGB value and non in exponent
+	print score
+	return score/(len(yarns_opt)*len(image_opt))
 	
 class yarn():
-	def __init__(self,start,finish,num_channels):
+	def __init__(self,start,finish):
 		self.start=start
 		self.finish=finish
-		self.channels=[]
-		for a in range(num_channels):
-			self.channels.append(True)
 	def setContributingPoints(self,dist,grid):
 		self.cont_points=[]
 		#make this the one that gives index
 		for i,point in enumerate(grid):
-			# if getPerpDistance(point,self.start,self.finish)<=dist:
-			self.cont_points.append((point,getPerpDistance(point,self.start,self.finish)))
-			point.num_pyarns=point.num_pyarns+1
-			point.pyarns.append(self)
+			if getPerpDistance(point,self.start,self.finish)<=dist:
+				self.cont_points.append((point,getPerpDistance(point,self.start,self.finish)))
+				print self.cont_points[0]
+				point.num_pyarns=point.num_pyarns+1
+				point.pyarns.append(self)
 		return grid
 
 class pixel():
@@ -79,13 +80,21 @@ class pixel():
 		self.b=blue
 		
 def main():
+	global yarns_opt,image_opt
 	args=parseArgs()
 	image=Image.open(args.image_filename)
 	image_matrix=np.asarray(image)
 	image_matrix.shape
 	image_grid=generateGrid(image_matrix.shape[0],image_matrix.shape[1],xscale=image_matrix.shape[0]/args.image_size,yscale=image_matrix.shape[0]/args.image_size,image_matrix=image_matrix)
 	image_grid,yarns=generateDecisionYarns(args,image_grid,image_matrix)
-	print getObjectiveFunction(yarns,image_matrix)
+	yoi = [random.randint(0,1) for yarn in yarns]
+
+	yarns_opt=yarns
+	image_opt=image_grid
+
+	result=minimize(getObjectiveFunction,yoi,method='BFGS')
+	print result.x
+	print result.message
 	   
 if __name__ == "__main__":
 	main()
